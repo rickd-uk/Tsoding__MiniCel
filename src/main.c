8a44429b6f9c1c4544c7c097a1daee2ea1c1ec99
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-09-30 15:36:02 +0900

    Add: file handling

--- src/main.c
+++ src/main.c
@@ -1,8 +1,62 @@
+#include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void usage(FILE *stream) {
-  fprintf(stream, "Usage: ./minicel <input.csv>\n");
+void usage(FILE *stream) { fprintf(stream, "Usage: ./minicel <input.csv>\n"); }
+
+char *slurp_file(const char *file_path, size_t *size) {
+  char *buffer = NULL;
+
+  FILE *f = fopen(file_path, "rb");
+  if (f == NULL) {
+    goto error;
+  }
+
+  if (fseek(f, 0, SEEK_END) < 0) {
+    goto error;
+  }
+
+  long m = ftell(f);
+  if (m < 0) {
+    goto error;
+  }
+
+  buffer = malloc(sizeof(char) * m);
+
+  if (buffer == NULL) {
+    goto error;
+  }
+
+  if (fseek(f, 0, SEEK_SET) < 0) {
+    goto error;
+  }
+  // return how much we read
+  size_t n = fread(buffer, 1, m, f);
+  assert(n == (size_t) m);
+
+  if (ferror(f)) {
+    goto error;
+  }
+  if (size) {
+    *size = n;
+  }
+
+  fclose(f);
+
+
+  return 0;
+
+error:
+  if (f) {
+    fclose(f);
+  }
+
+  if (buffer) {
+    free(buffer);
+  }
+  return NULL;
 }
 
 int main(int argc, char **argv) {
@@ -14,7 +68,14 @@ int main(int argc, char **argv) {
 
   const char *input_file_path = argv[1];
 
+  size_t content_size = 0;
 
+  char *content = slurp_file(input_file_path, &content_size);
+  if (content == NULL) {
+    fprintf(stderr, "ERROR: could not read file %s: %s\n", input_file_path,
+            strerror(errno));
+    exit(1);
+  }
 
   return 0;
 }
