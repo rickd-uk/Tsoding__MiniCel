868a9eca6c71577b22fda5f9587ac65253ca2772
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-09-30 14:41:54 +0900

    Add: Program Usage & Error Handling for input file

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  printf("Hello, minicel");
+void usage(FILE *stream) {
+  fprintf(stream, "Usage: ./minicel <input.csv>\n");
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    usage(stderr);
+    fprintf(stderr, "ERROR: input file has not been provided\n");
+    exit(1);
+  }
+
+  const char *input_file_path = argv[1];
+
+
+
   return 0;
 }
